767268bfccbd0d1900e8efb814932fa922499f11
Paul Kim <kimbaudi@gmail.com>
2021-04-13 08:46:14 -0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: 'title',
+    description: 'description',
+  },
+  flags: {
+    DEV_SSR: false,
+    FAST_DEV: false,
+    PRESERVE_FILE_DOWNLOAD_CACHE: true,
+    PRESERVE_WEBPACK_CACHE: true,
+  },
+  plugins: [],
+}
